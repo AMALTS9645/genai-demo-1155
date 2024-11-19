414917e3c38120a71c7288eb2d346a064a05d520
Sinajmon <you@example.comsinajmon.kalathiparambilaboo@deliveryoperations.onmicrosoft.com>
2024-11-19 16:47:26 +0000

    Completed

---
+++ src/main/java/com/example/demo/GenaiDemo1155Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiDemo1155Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiDemo1155Application.class, args);
+	}
+
+}
